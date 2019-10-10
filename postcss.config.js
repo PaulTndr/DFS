8298aa87fbbd616acf54b862e275a237d60bb4f5
Paul Tondereau <paul.tondereau@yahoo.fr>
2019-10-10 14:49:31 +0200

    pcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
+        require('postcss-import'),
         require('tailwindcss'),
+        purgecss({
+            content: ['./*.html']
+        }),
     ]
 };
